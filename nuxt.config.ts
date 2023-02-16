0b7a0181ade6cef96eb9ae5cc18b4717b6581d03
Atif <95560421+atiflayer@users.noreply.github.com>
2023-02-16 13:19:20 +0600

    Installed Vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// import { defineNuxtConfig } from 'nuxt'
 export default defineNuxtConfig({
-
+    // ssr: false,
+    css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
+    build: {
+        transpile: ['vuetify'],
+    },
 })
